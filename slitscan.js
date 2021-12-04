1eb62ea45dcba6e7ff89df104642f08f8d6a6722
Mike Romeo <michaelpromeo@gmail.com>
2021-12-04 12:17:42 -0500

    Add files via upload

---
+++ slitscan.js
@@ -0,0 +1,31 @@
+let capture;
+let x = 0; 
+let speed = 0;
+
+function setup() {
+  createCanvas(1080,1080);
+  capture = createCapture(VIDEO);
+  capture.size(800,600);
+  //capture.hide();
+}
+
+function draw() {
+  // background(255);
+  // image(capture, 0, 0, 320, 240);
+  // filter(INVERT);
+
+
+  let w = capture.width;
+  let h = capture.height; 
+  copy(capture, w/2, 0, 1, h, x, 0, 1, h);
+  x += speed;
+}
+
+function keyPressed(){
+ 
+  if (key == 'r') {
+   x = 0; 
+   speed = 1;
+  }
+
+}
