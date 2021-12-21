6329fd35c8657548aed017efaa761521f7d1489d
mromeolisten <mike@wearelisten.com>
2021-12-21 16:21:46 -0500

    added flip and loop

--- slitscan.js
+++ slitscan.js
@@ -1,6 +1,11 @@
+
 let capture;
 let x = 0; 
 let speed = 0;
+let bLoop = true; 
+let bFlip = false;
+let flipCounter = 0;
+
 
 function setup() {
   createCanvas(1080,1080);
@@ -16,8 +21,20 @@ function draw() {
 
   let w = capture.width;
   let h = capture.height; 
-  copy(capture, w/2, 0, 1, h, x, 0, 1, h);
+  
+  if(!bFlip) {
+    
+    copy(capture, w/2, 0, 1, h, x, 0, 1, h);
+  
+  } else {
+    
+    copy(capture, 0,w/2, h, 1, 0, x, h, 1);
+  }
   x += speed;
+
+  if (bLoop == true &&  x >= width) {
+    x = 0; 
+  }
 }
 
 function keyPressed(){
@@ -27,11 +44,30 @@ function keyPressed(){
    speed = 1;
   }
 
+  if (key == 'l') { 
+    bLoop = !bLoop; 
+    x = 0;
+    speed = 1;
+  }
+
+  if (key == 'f') {
+    bFlip = !bFlip;
+  }
+
+
+
 }
 
 function touchStarted() {
-
    x = 0; 
    speed = 1;
+
+   flipCounter++; 
+   flipCounter %= 2;
+   if (flipCounter == 1) {
+    bFlip = !bFlip;
+   }
+
+   console.log("flipCounter:  " +  flipCounter);
   return false;
 }
